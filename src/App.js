3fad789eb9e152db32434dacede9af9322a280cc
Aditi Davange <davangeaditi@gmail.com>
2022-02-24 12:59:32 +0530

    required code edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './index.css'
+import React from "react";
+import Project from "./Project"
+import Video1 from "./Video1.mp4";
+import Video2 from "./Video2.mp4";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{padding: "2% 5%"}}>
+            <h2 className="section-heading">Projects</h2>
+            <div className="container">
+                <Project h="Advance Image Cropping "
+                video={Video1} w1="100%" l1="Python"
+                    p1="Patent filed ✅"
+                    p2="A Desktop Application which can not only crop out margins but also end to end middle portion of a image."
+                    p3="Libraries used: PIL, tkinter"
+                />
+                <hr></hr>
+                 <Project h="PTG-Forum"
+                 video={Video2} w1="52.2%" w2="39.2%" w3="8.6%" l1="EJS" l2="Javascript" l3="CSS"
+                    p1="MongoDB Database"
+                    p2="A Chat Webapp that authorizes users and allows them to message anonymously."
+                    p3="Libraries used: Mongoose, Express, Passport"
+                />
+            
+            </div>
+        </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Carousel from "./Carousel";
+import Footer from "./Footer";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -10,7 +12,19 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+ReactDOM.render(
+  <React.StrictMode>
+    <h2 className="section-heading">Marksheets</h2>
+    <Carousel /> 
+  </React.StrictMode>,
+  document.getElementById('marksheets')
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('foot')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
